496075d474e90b2f3660156f575430c3e5e2136e
Caffeineholic <occ3710@outlook.jp>
2026-05-15 08:41:26 +0900

    write: 20260515-03

--- setup_abc.bat
+++ setup_abc.bat
@@ -4,33 +4,35 @@ setlocal enabledelayedexpansion
 :: ========================================
 :: 設定エリア：ここを書き換えるだけでOK
 :: ========================================
-set TARGET_DIR=ARC--219
+set TARGET_DIR=ABC300
 :: ========================================
 
 :: フォルダ名の設定 (例: ABC452)
 set SOURCE_FILE=main.py
 
 :: main.pyが存在するかチェック
-if not exist %SOURCE_FILE% (
-    echo [ERROR] %SOURCE_FILE% が見つかりません。
+if not exist "%SOURCE_FILE%" (
+    echo [ERROR] %SOURCE_FILE% is not found.
     pause
     exit /b
 )
 
-:: フォルダの作成
-if not exist %TARGET_DIR% (
-    mkdir %TARGET_DIR%
-    echo フォルダ %TARGET_DIR% を作成しました。
+:: フォルダの作成と存在チェック
+if not exist "%TARGET_DIR%" (
+    mkdir "%TARGET_DIR%"
+    echo folder %TARGET_DIR% was created.
 ) else (
-    echo [INFO] %TARGET_DIR% は既に存在します。
+    echo [ERROR] folder %TARGET_DIR% is exist and prosess stopped.
+    pause
+    exit /b
 )
 
 :: A.py から G.py までをコピー
 for %%F in (A B C D E F G) do (
     copy "%SOURCE_FILE%" "%TARGET_DIR%\%%F.py" > nul
-    echo   -^> %TARGET_DIR%\%%F.py を作成しました。
+    echo    -^> %TARGET_DIR%\%%F.py was created.
 )
 
 echo.
-echo 全てのファイルのコピーが完了しました！
+echo all file copied !
 pause
