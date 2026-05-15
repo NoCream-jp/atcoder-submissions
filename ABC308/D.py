6746bfc4e9d8d9bb2652703018931ae84a6606f1
Caffeineholic <occ3710@outlook.jp>
2026-05-15 11:05:05 +0900

    write: 20260515-05

--- ABC308/D.py
+++ ABC308/D.py
@@ -12,7 +12,7 @@ Here is my coding space
 # input = sys.stdin.readline
 # alpha = "abcdefghijklmnopqrstuvwxyz"
 # MOD = 998244353
-# drct = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+drct = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 def i_map():
@@ -36,12 +36,35 @@ from itertools import permutations as p
 
 ##################################################
 
-
 def main():
-
     
+    H, W = i_map()
+    grid = [input() for _ in range(H)]
+
+    snuke = "snuke"
+    start = (0, 0, 0) # i, j, idx
+    
+    stack = [start]
+    visited = [[False for j in range(W)] for i in range(H)]
+    visited[0][0] = True
+    while stack:
+        i, j, idx = stack.pop()
+        if i == H-1 and j == W-1:
+            print("Yes")
+            return
+        for di, dj in drct:
+            ni, nj = i + di, j + dj
+            nidx = (idx + 1) % 5
+            if 0 <= ni < H and 0 <= nj < W:
+                if grid[ni][nj] == snuke[nidx] and not visited[ni][nj]:
+                    stack.append((ni, nj, nidx))
+                    visited[ni][nj] = True
+                else:
+                    continue
+    print("No")
 
     return
+
 ######################################################
 
 if __name__ == "__main__":
