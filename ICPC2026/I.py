1024a4be1c511c9cddee638319dc41ffb3f45459
Caffeineholic <occ3710@outlook.jp>
2026-07-03 15:21:47 +0900

    write: 20260703-03

--- ICPC2026/I.py
+++ ICPC2026/I.py
@@ -1,7 +1,4 @@
 ###################################################
-# import sys
-# sys.setrecursionlimit(10 ** 7)
-# input = sys.stdin.readline
 # alpha = "abcdefghijklmnopqrstuvwxyz"
 # MOD = 998244353
 # drct = [(0, 1), (1, 0), (0, -1), (-1, 0)]
@@ -18,14 +15,6 @@ def i_list():
 def c_list():
     return list(input().split())
 
-# from collections import defaultdict
-# from collections import Counter
-# from sortedcontainers import SortedList
-# from collections import deque
-# import heapq
-# import math
-# import bisect
-# from itertools import permutations
 
 ##################################################
 
