f845139197c7839f18441c2f8a7b615bfefbe33e
Caffeineholic <occ3710@outlook.jp>
2026-07-11 20:03:36 +0900

    write: 20260711-04

--- setup_abc.bat
+++ setup_abc.bat
@@ -1,19 +1,31 @@
 @echo off
+:: ↓この1行を追加する（文字コードをUTF-8にするコマンド）
+chcp 65001 > nul
+
 setlocal enabledelayedexpansion
 
 :: ========================================
-:: 設定エリア：ここを書き換えるだけでOK
+:: 設定エリア
 :: ========================================
-set TARGET_DIR=ABC149
-
-:: ========================================
-
-:: フォルダ名の設定 (例: ABC452)
 set SOURCE_FILE=main.py
+
+:: 実行時にフォルダ名を入力させる
+set /p TARGET_DIR="作成するコンテスト名を入力してください (例: ABC149): "
+
+:: (以下略)
+
+:: 空白のままEnterを押された場合の処理
+if "%TARGET_DIR%"=="" (
+    echo [ERROR] フォルダ名が入力されていません。
+    pause
+    exit /b
+)
+
+:: ========================================
 
 :: main.pyが存在するかチェック
 if not exist "%SOURCE_FILE%" (
-    echo [ERROR] %SOURCE_FILE% is not found.
+    echo [ERROR] "%SOURCE_FILE%" が見つかりません。
     pause
     exit /b
 )
@@ -21,9 +33,9 @@ if not exist "%SOURCE_FILE%" (
 :: フォルダの作成と存在チェック
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
-    echo folder %TARGET_DIR% was created.
+    echo フォルダ "%TARGET_DIR%" を作成しました。
 ) else (
-    echo [ERROR] folder %TARGET_DIR% is exist and prosess stopped.
+    echo [ERROR] フォルダ "%TARGET_DIR%" は既に存在するため、処理を中止します。
     pause
     exit /b
 )
@@ -31,9 +43,9 @@ if not exist "%TARGET_DIR%" (
 :: A.py から G.py までをコピー
 for %%F in (A B C D E F G) do (
     copy "%SOURCE_FILE%" "%TARGET_DIR%\%%F.py" > nul
-    echo    -^> %TARGET_DIR%\%%F.py was created.
+    echo    -^> "%TARGET_DIR%\%%F.py" を作成しました。
 )
 
 echo.
-echo all file copied !
+echo すべてのファイルのコピーが完了しました！
 pause
